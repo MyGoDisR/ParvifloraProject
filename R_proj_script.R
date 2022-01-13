ee7a24c01e96dbb38c760b1e165563044f945d27
Paweł Jędrzejczak <pawel@MacBook-Pro-Pawe.local>
2022-01-13 21:41:20 +0100

    Initial commit Adding R project file and Notes

---
+++ R_proj_script.R
@@ -0,0 +1,6 @@
+sales_feb_2020 <- read.csv("Summary of Sales February 2020.csv")
+sales_jan_2020 <- read.csv("Summary of Sales January 2020.csv")
+sales_mar_2020 <- read.csv("Summary of Sales March 2020.csv")
+stores <- readxl::read_excel("Stores.xlsx")
+
+# Michau to jest gosc
