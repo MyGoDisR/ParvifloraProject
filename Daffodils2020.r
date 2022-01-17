dc6bfb3596476d2360daf47c1e83647e35cd07d2
MattG133 <maciek.golik07@gmail.com>
2022-01-17 13:27:19 +0100

    Zmiana funkcji tak aby działały z plikami xls oraz dodanie funkcji które mergują tabele - dodanie miesięcy do tabeli - małe zmiany estetyczne w skrypcie, żeby łatwiej było w nim operować - funkcjia parse_xls_table ma teraz argument pivot, któy jest opisany w skrypcie - funkcja parse_summary_table(czy jakoś tak) działa z plikiem xls - NOWA funckja combine_tables łączy tabele uzyskane z parse_xls_table dla wszystkich sheetów - funkcja merge_summaries - poprawiono kilka elementów i dostosowano do nowych funkcji

--- Daffodils2020.r
+++ Daffodils2020.r
@@ -3,8 +3,14 @@ library(tidyverse)
 library(tidyxl)
 library(rio)
 
-xlsx_path <- '/Users/Maciek/Desktop/Master_Studies/Itroduction_to_programming_with_R/R_Final_Project/R_proj/Daffodils2020.xlsx'
-xls_path <- ''
+#INPUTS:
+##################################################################################################################################################################################
+xls_path <- 'Daffodils2020.xls'
+
+
+
+#FUNCTIONS:
+##################################################################################################################################################################################
 
 get_file_paths <- function(pattern = "Daffodils*.xls"){
   '
@@ -24,99 +30,53 @@ get_file_paths <- function(pattern = "Daffodils*.xls"){
   return(daffodils_file_paths)
 }
 
-convert_to_xlsx <- function(xls_path) {
-  '
-  Function converts the Daffodil file into .xlsx format so it could be processed by tidyxl package
-  input:
-    xls_path (str) - path to the xls file we want to convert
-  returns:
-    conv_file (str) - name of the converted .xlsx file
-  
-  TODO: yr could also possibly be returned and then used to name the data.frame if there are multiple years
-  '
-  
-  #UNNEEDED WITH parse_xls_table which handles xls files
-  
-  # import all sheets from the file as tibbles stored in a list
-  temp_xls <- rio::import_list(xls_path, setclass = "tbl")
-  yr <- stringr::str_match(xls_path, "Daffodils\\s*(.*?)\\s*.xls")[,2]
-  # export it as .xlsx file (the sheets go into their place)
-  conv_file = paste("Daffodils",  yr, ".xlsx", sep='')
-  rio::export(temp_xls, conv_file)
-  return(conv_file)
-}
 
-
-get_sheet_names <- function(path){
-  #This function returns names of all the sheets in the excel files
-  #inputs:
-  #path - path of a xlsx file
-  
-  cells <- xlsx_cells(path)
-  
-  return(unique(cells$sheet))
-}
-
-summary_for_period <- function(path, sheet_name){
-  ##This function retrieves and formats summary_for_period table from Parviflora Financial Report Excel File
+parse_period_summary <- function(path, sheet_name){
+ '
+ function retrieves and formats summary_for_period table from Parviflora Financial Report Excel File
   ###Inputs:
-  ###path - path of a xlsx Parviflora Report file
+  ###path - path of a excel Parviflora Report file
   ###sheet_name - name of a sheet from which the table is to be retrieved
+  '
   
+  #creating excel object
+  df <- read_excel(path, sheet=sheet_name)
   
-  #creating cells object
-  cells <- xlsx_cells(path)
-    
-  #filtering out unnecessary data
-  summary <- cells %>%
-  filter(sheet==sheet_name) %>%
-  select(sheet, character, numeric, date, row, col, data_type)
-    
+  #filtering out unnecessary data and renaming columns, removing rows with NAs in labels column 
+  df <- df %>% rename(labels=1, trans_amount=4, trans_count=5) %>% select(1,4,5) %>% filter(!is.na(labels))
+  
   #defining the values of the beginning cell and ending cell of summary table
   first_cell <- 'Summary for Period'
   last_cell <- 'Summary Totals'
-   
-  #creating a helper table with just the row labels and row and col indexes from the chosen excel sheet
-  position_and_row <- summary %>% filter(!is.na(character)) %>% select(character, row, col)
-    
+  
+  
   #retrieving the coordinates of the first row label and last row labels of a summary_for_period table
-  beginning_row <- stack(setNames(lapply(position_and_row, grep, pattern = first_cell), 'character'))[[1]]
-  end_row <- stack(setNames(lapply(position_and_row, grep, pattern = last_cell), 'character'))[[1]]
-    
-  #selecting only rows that lie between beginning row and end_row, which are part of the summary_for_period table
-  position_and_row <- position_and_row %>% slice(beginning_row:end_row) %>% filter(col==1)
-    
-  #selecting a helper column with rows to lookup the corresponding values  
-  rows <- position_and_row %>% select(row)
+  beginning_row <- stack(setNames(lapply(df, grep, pattern = first_cell), 'labels'))[[1]]
+  end_row <- stack(setNames(lapply(df, grep, pattern = last_cell), 'labels'))[[1]]
   
-  #creating labels column (removing the first row because it is a name of a table without corresponding value)  
-  position <- position_and_row %>% slice(-1) %>% select(character)
+  #selecting only rows that lie between beginning row and end_row, which are start and end of the summary_for_period table
+  df <- df %>% slice((beginning_row+1):end_row)
   
-  #creating column with values by looking up the values corresponding to rows at which labels lie
-  trans <- merge(rows, summary[c('numeric', 'row', 'col')], on=row)
+  #converting value columns to numeric format and changing null values to 0
+  df <- df %>% mutate(trans_count=as.numeric(trans_count)) %>% mutate(trans_amount=as.numeric(trans_amount)) %>% mutate_if(is.numeric, ~replace(., is.na(.), 0))
   
-  #saving each value column to a separate variable
-  trans_amount <- trans %>% filter(col==4) %>% slice(-1)
-  trans_count <- trans %>% filter(col==5) %>% slice(-1)
-  
-  #creating a dataframe with the three columns  
-  df <- data.frame(position, trans_amount$numeric, trans_count$numeric)
-  
-  #renaming the columns
-  df <- df %>% rename(position=character, trans_amount=trans_amount.numeric, trans_count=trans_count.numeric)
-    
   return(df)
 }
 
-parse_xls_table <- function(path, sheet){
-  #TODO: remove '--' in values 
-  #This function takes Parviflora Financial Report (part with each shops' report) and bundles it together in one table
+
+parse_xls_table <- function(path, sheet, pivot=TRUE){
+  '
+  #TODO: remove -- in values 
+  #This function takes Parviflora Financial Report (part with each shops report) and bundles it together in one table
   #inputs:
   #path - path of a excel file
   #sheet - name or index of a excel sheet
+  #pivot (TRUE/FALSE), default:TRUE - if TRUE function returns pivoted table (if no other major transformations are to be done), if FALSE function returns unpivoted table (if other transformations are to be applied to it later on, i.e. merging with other months)
+  '
+  
   
   #Reading in the data
-  df <- read_excel(file_path, sheet = sheet_name) # Equivalent
+  df <- read_excel(path, sheet = sheet) # Equivalent
   df <- df %>% rename(labels=1, val1=2, val2=3, val3=4, val4=5)
   
   #setting the value of a first label cell in each individual report to later search for coordinates
@@ -169,7 +129,8 @@ parse_xls_table <- function(path, sheet){
   #There is a situation where labels duplicate, in those instances value for one label is neagative, because all labels in each table need to be unique,
   #in the code below sufixes are added to labels with negative values
   df_trimmed <- df %>%
-    mutate(trans_amount = replace(trans_amount, is.na(trans_amount), 0)) %>%
+    mutate(trans_amount = as.numeric(trans_amount), trans_count=as.numeric(trans_count)) %>%
+    mutate_if(is.numeric, ~replace(., is.na(.), 0)) %>%
     mutate(label_suffix = case_when(trans_amount<0 ~ ' (neg)', TRUE ~ '')) %>%
     mutate(labels = paste(labels, label_suffix)) %>% select(-label_suffix)
   
@@ -187,12 +148,125 @@ parse_xls_table <- function(path, sheet){
   #creating a stacked tables
   df_final <- rbind(df_trans_amount, df_trans_count)
   
-  #pivoting the final table
-  df_final_pivot <- spread(df_final, labels, value)
-  df_final_pivot <- df_final_pivot %>% select(-names(a)[16])
+  #adding month column
   
-  return(a)
+  df_final$month <- sheet
+  
+  #if pivot argument is true, the function pivots df_final and returns the pivoted frame, else it return just df_final - this can be used if later on all months are to be combined in one table
+  if (pivot==TRUE) {
+    
+    #pivoting the final table
+    df_final_pivot <- spread(df_final, labels, value)
+    df_final_pivot <- df_final_pivot %>% select(-names(df_final_pivot)[16])
+    
+    return(df_final_pivot)
+    
+  } else {
+    
+    df_final <- df_final %>% slice(-nrow(df_final))
+    return(df_final)
+    
+  }
 }
+
+
+merge_summaries <- function(xlsx_path){
+  '
+  #this function creates one summary-for_period combining each individual summary from one sheet of the xls file
+  #input
+  #xlsx_path - path to xlsx file
+  '
+  
+  #extracting names of sheets (months)
+  sheets <- excel_sheets(xlsx_path)
+  
+  #creating an empty table to fill with data using for loop
+  df <- data.frame(position=c(), trans_amount=c(), trans_count=c())
+  
+  #looping through sheets and creating summaries, then binding them together
+  for (s in sheets){
+    df_1 <- parse_period_summary(xlsx_path, s)
+    df_1$month <- s
+    df <- rbind(df, df_1)
+  }
+  
+  #changing names of duplicate labels with negative values by adding suffix (neg)
+  df_trimmed <- df_2 %>%
+    mutate(label_suffix = case_when(trans_amount<0 ~ ' (neg)', TRUE ~ '')) %>%
+    mutate(labels = paste(labels, label_suffix)) %>% select(-label_suffix)
+  
+  #separating df to two with separate values in each to later combine to one df with only one value column
+  df_trans_amount <- df_trimmed %>% select(-trans_count) %>% mutate(value_label='trans_amount') %>% rename(values=trans_amount)
+  df_trans_count <- df_trimmed %>% select(-trans_amount) %>% mutate(value_label='trans_count') %>% rename(values=trans_count)
+  
+  #binding the two df together and spreading the df to simplify it
+  df <- rbind(df_trans_amount, df_trans_count) %>% spread(labels, values)
+  
+  return(df)
+}
+
+
+combine_tables <- function(path){
+  
+  '
+  Function loops through all sheets in xls file and creates tabels, then binds them togheter and pivots into one final
+  Inputs:
+  path - path to excel file
+  '
+  
+  #extracting sheet names
+  sheets <- excel_sheets(path)
+  
+  #creating empty dataframe to later fill in
+  df <- data.frame(labels=c(), value=c(), id=c(), code=c(), position=c(), month=c())
+  
+  #looping through all sheets in the excel file, creating tabels with financial data and binding them all together 
+  for (s in sheets) {
+    df_1 <- parse_xls_table(path, s, pivot=FALSE)
+    df <- rbind(df, df_1)
+  }
+  
+  #pivoting the table
+  df_pivoted <- df %>% spread(labels, value)
+  
+  return(df_pivoted)
+}
+
+
+#CODE:
+##################################################################################################################################################################################
+
+# Define Constants (just for now) - the future loop
+sheet_name <- 'Feb20'
+
+###TODO: add store id and month
+### Minor improvement: Change functions a bit because we create the object from xlsx_cells() few times and it could be reused 
+### DONE convert xls to xlsx 
+### looping through all sheets
+
+paths <- get_file_paths()
+# TODO For now we only have one year - thus one file [1] but what in the future?
+# Create a loop to iterate through these files
+
+#unneeded now
+#conv_file <- convert_to_xlsx(paths[1])
+
+# commented as it is used inside summary_for_period function
+#cells <- xlsx_cells(conv_file)
+
+df_summary <- merge_summaries(paths) #change to conv_file or even that cells object
+#TODO: change summary_for_period function so it can take in xls files
+
+df <- parse_xls_table(paths, sheet_name) #change to conv_file 
+#DONE: remove '--' in values - replace with 0s or NAs
+
+df_final <- combine_tables(paths)
+
+
+
+
+#DEPRECATED FUNCTIONS: DELETE?
+########################################################################################################################################################################
 
 parse_xlsx_table <- function(xlsx_path, sheet_name){
   #FUNCTION IS OLD AND SHOULD BE DELETED LATER ON
@@ -307,42 +381,34 @@ parse_xlsx_table <- function(xlsx_path, sheet_name){
   return(a)
 }
 
-
-merge_summaries <- function(xlsx_path){
-  #this function creates one summary-for_period combining each sheet of the xlsx file
-  #input
-  #xlsx_path - path to xlsx file
+convert_to_xlsx <- function(xls_path) {
+  '
+  Function converts the Daffodil file into .xlsx format so it could be processed by tidyxl package
+  input:
+    xls_path (str) - path to the xls file we want to convert
+  returns:
+    conv_file (str) - name of the converted .xlsx file
   
-  sheets <- get_sheet_names(xlsx_path)
+  TODO: yr could also possibly be returned and then used to name the data.frame if there are multiple years
+  '
   
-  df_2 <- data.frame(position=c(), trans_amount=c(), trans_count=c())
-
-  for (s in sheets){
-    df_1 <- summary_for_period(xlsx_path, s)
-    df_1$sheet <- s
-    df_2 <- rbind(df_2, df_1)
-  }
-  return(df_2)
+  #UNNEEDED WITH parse_xls_table which handles xls files
+  
+  # import all sheets from the file as tibbles stored in a list
+  temp_xls <- rio::import_list(xls_path, setclass = "tbl")
+  yr <- stringr::str_match(xls_path, "Daffodils\\s*(.*?)\\s*.xls")[,2]
+  # export it as .xlsx file (the sheets go into their place)
+  conv_file = paste("Daffodils",  yr, ".xlsx", sep='')
+  rio::export(temp_xls, conv_file)
+  return(conv_file)
 }
 
-# Define Constants (just for now) - the future loop
-sheet_name <- 'Feb20'
-
-###TODO: add store id and month
-### Minor improvement: Change functions a bit because we create the object from xlsx_cells() few times and it could be reused 
-### DONE convert xls to xlsx 
-### looping through all sheets
-
-paths <- get_file_paths()
-# TODO For now we only have one year - thus one file [1] but what in the future?
-# Create a loop to iterate through these files
-conv_file <- convert_to_xlsx(paths[1])
-
-# commented as it is used inside summary_for_period function
-#cells <- xlsx_cells(conv_file)
-
-df_summary <- merge_summaries(xlsx_path) #change to conv_file or even that cells object
-#TODO: change summary_for_period function so it can take in xls files
-
-df <- parse_xls_table(xlsx_path, sheet_name) #change to conv_file 
-#TODO: remove '--' in values - replace with 0s or NAs
+get_sheet_names <- function(path){
+  #This function returns names of all the sheets in the excel files
+  #inputs:
+  #path - path of a xlsx file
+  
+  cells <- xlsx_cells(path)
+  
+  return(unique(cells$sheet))
+}
