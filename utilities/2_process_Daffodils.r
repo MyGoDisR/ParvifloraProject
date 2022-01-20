3426cc7ea0fcffb7ffc9808ddc8991e33b65c4fd
MattG133 <maciek.golik07@gmail.com>
2022-01-20 15:02:31 +0100

    -dodano argument totals_only do funkcji parse_xls_table() i parse_period_summary(), który decyduje czy funkcja zwróci tabelę z samymi totalami czy ze wszystkim -do funkcji parse_xls_table dodano kod który tworzy kolumny miesiąc i rok (numer) -w funkcji parse_xls_table zmieniono nazwę kolumny code na store_id -usunięto argument pivot z funkcji parse_xls_table, gdyż zbytnio komplikował kod, a nie jest potrzebny -uproszczono niektóre działania -poprawiono layout

--- utilities/2_process_Daffodils.r
+++ utilities/2_process_Daffodils.r
@@ -25,19 +25,26 @@ get_file_paths <- function(pattern = "Daffodils*.xls"){
 }
 
 
+
+
 parse_period_summary <- function(path, sheet_name){
- '
- function retrieves and formats summary_for_period table from Parviflora Financial Report Excel File
-  ###Inputs:
-  ###path - path of a excel Parviflora Report file
-  ###sheet_name - name of a sheet from which the table is to be retrieved
+  '
+  function retrieves and formats summary_for_period table from Parviflora Financial Report Excel File
+  Inputs:
+    -> path - path of a excel Parviflora Report file
+    -> sheet_name - name of a sheet from which the table is to be retrieved
+  Output:
+    <- df - data.frame object with summary for period for one month
   '
   
   #creating excel object
-  df <- read_excel(path, sheet=sheet_name)
+  df <- readxl::read_excel(path, sheet=sheet_name)
   
   #filtering out unnecessary data and renaming columns, removing rows with NAs in labels column 
-  df <- df %>% rename(labels=1, trans_amount=4, trans_count=5) %>% select(1,4,5) %>% filter(!is.na(labels))
+  df <- df %>%
+    dplyr::rename(labels=1, trans_amount=4, trans_count=5) %>%
+    dplyr::select(1,4,5) %>%
+    dplyr::filter(!is.na(labels))
   
   #defining the values of the beginning cell and ending cell of summary table
   first_cell <- 'Summary for Period'
@@ -49,187 +56,218 @@ parse_period_summary <- function(path, sheet_name){
   end_row <- stack(setNames(lapply(df, grep, pattern = last_cell), 'labels'))[[1]]
   
   #selecting only rows that lie between beginning row and end_row, which are start and end of the summary_for_period table
-  df <- df %>% slice((beginning_row+1):end_row)
+  df <- df %>% dplyr::slice((beginning_row+1):end_row)
   
   #converting value columns to numeric format and changing null values to 0
-  df <- df %>% mutate(trans_count=as.numeric(trans_count)) %>% mutate(trans_amount=as.numeric(trans_amount)) %>% mutate_if(is.numeric, ~replace(., is.na(.), 0))
+  df <- df %>%
+    dplyr::mutate(trans_count=as.numeric(trans_count)) %>%
+    dplyr::mutate(trans_amount=as.numeric(trans_amount)) %>%
+    dplyr::mutate_if(is.numeric, ~replace(., is.na(.), 0))
   
   return(df)
 }
 
 
-parse_xls_table <- function(path, sheet, pivot=TRUE){
+
+
+parse_xls_table <- function(path, sheet, totals_only=TRUE) {
   '
-  #TODO: remove -- in values 
-  #This function takes Parviflora Financial Report (part with each shops report) and bundles it together in one table
-  #inputs:
-  #path - path of a excel file
-  #sheet - name or index of a excel sheet
-  #pivot (TRUE/FALSE), default:TRUE - if TRUE function returns pivoted table (if no other major transformations are to be done), if FALSE function returns unpivoted table (if other transformations are to be applied to it later on, i.e. merging with other months)
+  This function takes Parviflora Financial Report (part with each shops report) and bundles it together in one table,
+  performing neccessary transformations and additions along the way
+  
+  inputs:
+    -> path - relative path of a excel file
+    -> sheet - name or index of a excel sheet from which the data is to be extracted
+    -> totals_only (TRUE/FALSE) -  default:TRUE - if TRUE function returns only rows with label Totals
+                                   if FALSE function returns all rows
+  outputs:
+    <- df - data.frame object with all Financial Report positions or just totals (depending on the totals_only argument) in a "tidy" format 
   '
   
-  # TODO rio package gives the option to read the whole file and store read-in sheets as data.frames inside a list
   #Reading in the data
-  df <- read_excel(path, sheet = sheet) # Equivalent
-  df <- df %>% rename(labels=1, val1=2, val2=3, val3=4, val4=5)
+  df <- readxl::read_excel(path, sheet = sheet) # Equivalent
+  df <- df %>% dplyr::rename(labels = 1, val1 = 2, val2 = 3, val3 = 4, val4 = 5)
   
-  #setting the value of a first label cell in each individual report to later search for coordinates
+  #Setting the value of a first label cell in each individual report to later search for coordinates
   first_cell <- 'Submitting Location:'
   
-  #creating a vector with locations of each row containing first_cell value
-  beginning_row <- stack(setNames(lapply(df, grep, pattern = first_cell), 'labels'))[[1]]
+  #Creating a vector with locations of each row containing first_cell value
+  start_row <- stack(setNames(lapply(df, grep, pattern = first_cell), 'labels'))[[1]]
   
-  #removing rows with summary table
-  df <- df %>% slice(beginning_row[1]:nrow(df)) %>% select(-c(4,5))
+  #Removing rows with summary table
+  df <- df %>% dplyr::slice(start_row[1]:nrow(df)) %>% select(-c(4,5))
   
-  #creating helper table with all rows labels and row and col indexes
-  labels <- df %>% filter(!is.na(labels))
+  #Creating helper table with all rows labels and row and col indexes
+  labels <- df %>% dplyr::filter(!is.na(labels))
   
   
-  #creating a vector with locations of each row containing first_cell value
-  #there is one uneeded row in each shop's table, which is
-  #submitting location
-  #code below removes it
+  #Creating a vector with locations of each row containing first_cell value, although
+  #there is one uneeded row in each shop's table, which is submitting location
   
-  #creating a vector with locations of each row containing first_cell value
+  #Creating a vector with locations of each row containing first_cell value
   loc <- stack(setNames(lapply(labels, grep, pattern = first_cell), 'labels'))[[1]]
   
-  #removing unneeded rows based on the locations calculated above 
-  labels_2 <- labels %>% slice(-c(loc))
+  #Removing unneeded rows based on the locations calculated above 
+  labels_final <- labels %>% dplyr::slice(-c(loc))
   
-  df <- labels_2 %>% rename(trans_amount=2, trans_count=3)
+  #Creating table with trans_amount and trans_count columns
+  df <- labels_final %>% dplyr::rename(trans_amount = 2, trans_count = 3 )
   
-  #extracting ids and codes
-  additional_col_names <- labels %>% slice(c(loc)) %>% rename(id=2, code=3) %>% select(-1)
+  #Extracting ids and codes
+  id_code <- labels %>%
+    dplyr::slice(c(loc)) %>%
+    dplyr::rename(id = 2, store_id = 3) %>%
+    dplyr::select(-1)
   
-  #putting ids in rows where labels == 'Totals' beceause this is the value that is exatcly the same in each table and is there always
-  value_to_search_for <- 'Totals'
-  intervals <- stack(setNames(lapply(df, grep, pattern = value_to_search_for), 'labels'))[[1]]
+  #Putting ids in rows where labels == 'Totals' beceause this is the value that is exatcly the same in each table and is there always
+  search_char <- 'Totals'
+  intervals <- stack(setNames(lapply(df, grep, pattern = search_char), 'labels'))[[1]]
   
-  #creating empty id and code column to later fill them in with corresponding data
+  #Creating empty id and code column to later fill them in with corresponding data
   df$id <- NA
-  df$code <- NA
+  df$store_id <- NA
   
-  #looping through all rows with 'Total' in labels column and putting a corresponding id and code in respective columns
+  #Looping through all rows with 'Total' in labels column and putting a corresponding id and code in respective columns
   for (i in 1:length(intervals)) {
-    df[intervals[i],'id'] <- additional_col_names$id[i]
-    df[intervals[i], 'code'] <- additional_col_names$code[i]
+    df[intervals[i],'id'] <- id_code$id[i]
+    df[intervals[i], 'store_id'] <- id_code$store_id[i]
   }
   
-  #filling empty rows in id columns with ids 
-  df <- df %>% fill(id, .direction='up') #%>% filter(!character=='--')
-  df <- df %>% fill(code, .direction='up')
+  #Filling empty rows in id and code columns with ids and codes corresponding to them
+  df <- df %>% tidyr::fill(c(id,store_id), .direction = 'up') #%>% filter(!character=='--')
+  
   
   #There is a situation where labels duplicate, in those instances value for one label is neagative, because all labels in each table need to be unique,
-  #in the code below sufixes are added to labels with negative values
-  df_trimmed <- df %>%
-    mutate(trans_amount = as.numeric(trans_amount), trans_count=as.numeric(trans_count)) %>%
-    mutate_if(is.numeric, ~replace(., is.na(.), 0)) %>%
-    mutate(label_suffix = case_when(trans_amount<0 ~ ' (neg)', TRUE ~ '')) %>%
-    mutate(labels = paste(labels, label_suffix)) %>% select(-label_suffix)
+  #in the code below sufixes are added to labels with negative values and also neccessary tidying is applied
+  df <- df %>%
+    dplyr::mutate(trans_amount = as.numeric(trans_amount), trans_count = as.numeric(trans_count)) %>% #converting value columns to numeric format
+    dplyr::mutate_if(is.numeric, ~replace(., is.na(.), 0)) %>% #replacing every NA in numeric column with 0
+    dplyr::mutate(label_suffix = case_when(trans_amount<0 ~ ' (neg)', TRUE ~ '')) %>% #creating suffix to duplicate labels with neg tot_amnt
+    dplyr::mutate(labels = paste(labels, label_suffix)) %>% # inserting suffix to labels
+    dplyr::select(-label_suffix) %>% #deleting suffix column
+    dplyr::mutate(labels = trimws(labels, which = c('both'))) %>% #removing trailing and leading spaces from labels
+    tidyr::drop_na(id) %>% #dropping rows where there is no id
+    dplyr::mutate(store_id=as.numeric(store_id)) #converting store_id to numeric format
+  
   
   #To make the final table there needs to be only one value column, therefore trans_amount and trans_count are going to be merged
-  # and additional label column is going to be created
-  df_trans_amount <- df_trimmed %>% select(labels, trans_amount, id, code)
-  df_trans_count <- df_trimmed %>% select(labels, trans_count, id, code)
+  #and additional label column is going to be created 
+  df<- df %>% tidyr::pivot_longer(trans_amount:trans_count, names_to = 'position', values_to = 'value')
   
-  df_trans_amount <- df_trans_amount %>% rename(value=trans_amount)
-  df_trans_count <- df_trans_count %>% rename(value=trans_count)
+  #Adding month and year column extracted from the sheet name
+  #WARNING: this approach will only work for the next 78 years, after that year column will yield unreliable results. In fact, does it yield reliable results now? What if the year is in fact 1920?!
+  df$month <- match(stringr::str_sub(sheet,1,3),month.abb)
+  df$year <- stringr::str_sub(sheet, -2, -1)
   
-  df_trans_amount$position <- 'trans_amount'
-  df_trans_count$position <- 'trans_count'
-  
-  #creating a stacked tables
-  df_final <- rbind(df_trans_amount, df_trans_count)
-  
-  #adding month column
-  # For the purpose of doing analysis and joining data.frames 
-  # it would be perhaps better to have separate columns for month and year
-  month_number <- match(substr(sheet,1,3),month.abb)
-  
-  # For now month column is in format "Feb20"
-  df_final$month <- sheet
-  
-  #if pivot argument is true, the function pivots df_final and returns the pivoted frame, else it return just df_final - this can be used if later on all months are to be combined in one table
-  if (pivot==TRUE) {
+  if (totals_only==TRUE) {
     
-    #pivoting the final table
-    df_final_pivot <- spread(df_final, labels, value)
-    df_final_pivot <- df_final_pivot %>% select(-names(df_final_pivot)[16])
+    df <- df %>% dplyr::filter(labels == 'Totals')
     
-    return(df_final_pivot)
+    return(df)
     
   } else {
     
-    df_final <- df_final %>% slice(-nrow(df_final))
-    return(df_final)
+    return(df)
     
   }
+  
+  #MESSAGE: Decided to delete the PIVOT functionality, since with totals_only argument it would produce too complicated code imo, moreover the pivoted table is not needed for our analysis
+  #although if in the future it would be needed it should be applied in a separate function.
+  
+  # #If pivot argument is true, the function pivots df_final and returns the pivoted frame, else it return just df_final - this can be used if later on all months are to be combined in one table
+  # if (pivot == TRUE) {
+  #   
+  #   #Pivoting the final table
+  #   df_final_pivot <- tidyr::spread(df, labels, value)
+  #   df_final_pivot <- df_final_pivot %>% dplyr::mutate_if(is.numeric, ~replace(., is.na(.), 0))
+  #   
+  #   return(df_final_pivot)
+  #   
+  # } else {
+  #     
+  #   return(df)
+  #   
+  # }
 }
 
 
-merge_summaries <- function(xlsx_path){
+
+merge_summaries <- function(path, totals_only=TRUE){
   '
-  #this function creates one summary-for_period combining each individual summary from one sheet of the xls file
-  #input
-  #xlsx_path - path to xlsx file
+  this function creates one summary-for_period combining each individual summary from one sheet of the xls file
+  Inputs:
+    -> xlsx_path - path to xlsx file
+    -> totals_only (TRUE/FALSE) - if TRUE returns only rows corresponding to "Summary Totals" label, if FALSE returns all rows
+  Output:
+    <- df - data.frame with summary for period table for all months
   '
   
   #extracting names of sheets (months)
-  sheets <- excel_sheets(xlsx_path)
+  sheets <- readxl::excel_sheets(path)
   
   #creating an empty table to fill with data using for loop
   df <- data.frame(position=c(), trans_amount=c(), trans_count=c())
   
   #looping through sheets and creating summaries, then binding them together
   for (s in sheets){
-    df_1 <- parse_period_summary(xlsx_path, s)
+    df_1 <- parse_period_summary(path, s)
     df_1$month <- s
     df <- rbind(df, df_1)
   }
   
   #changing names of duplicate labels with negative values by adding suffix (neg)
   df_trimmed <- df %>%
-    mutate(label_suffix = case_when(trans_amount<0 ~ ' (neg)', TRUE ~ '')) %>%
-    mutate(labels = paste(labels, label_suffix)) %>% select(-label_suffix)
+    dplyr::mutate(label_suffix = case_when(trans_amount<0 ~ ' (neg)', TRUE ~ '')) %>%
+    dplyr::mutate(labels = paste(labels, label_suffix)) %>% select(-label_suffix)
   
-  #separating df to two with separate values in each to later combine to one df with only one value column
-  df_trans_amount <- df_trimmed %>% select(-trans_count) %>% mutate(value_label='trans_amount') %>% rename(values=trans_amount)
-  df_trans_count <- df_trimmed %>% select(-trans_amount) %>% mutate(value_label='trans_count') %>% rename(values=trans_count)
+  #putting trans_count and trans_amount into one value column and 
+  df <- df %>% tidyr::pivot_longer(trans_amount:trans_count, names_to = 'value_label', values_to = 'values')
   
-  #binding the two df together and spreading the df to simplify it
-  df <- rbind(df_trans_amount, df_trans_count) %>% spread(labels, values)
+  if (totals_only==TRUE) {
+    
+    df <- df %>% dplyr::filter(labels=='Summary Totals')
+    return(df)
+    
+  } else {
+    
+    return(df)  
+    
+  }
   
-  return(df)
 }
 
 
-combine_tables <- function(path){
+
+combine_tables <- function(path, totals_only=TRUE){
   
   '
   Function loops through all sheets in xls file and creates tabels, then binds them togheter and pivots into one final
   Inputs:
-  path - path to excel file
+    -> path - path to excel file
+    -> totals_only (TRUE/FALSE) - if TRUE returns object with only Totals, if FALSE returns object with all Financial Report positions
+  Output:
+    <- df - data.frame object with totals for Daffodils for each month in a "tidy" format
   '
   
-  #extracting sheet names
-  sheets <- excel_sheets(path)
+  #Extracting sheet names
+  sheets <- readxl::excel_sheets(path)
   
-  #creating empty dataframe to later fill in
+  #Creating empty dataframe to later fill in
   df <- data.frame(labels=c(), value=c(), id=c(), code=c(), position=c(), month=c())
   
-  #looping through all sheets in the excel file, creating tabels with financial data and binding them all together 
+  #Looping through all sheets in the excel file, creating tabels with financial data and binding them all together 
   for (s in sheets) {
-    df_1 <- parse_xls_table(path, s, pivot=FALSE)
+    df_1 <- parse_xls_table(path, s, totals_only=totals_only)
     df <- rbind(df, df_1)
   }
   
-  #pivoting the table
-  df_pivoted <- df %>% spread(labels, value)
+  #pivoting the table and replacing NAs in numeric columns (if there are any) with 0s
+  df_pivoted <- df %>%
+    tidyr::spread(labels, value) %>%
+    dplyr::mutate_if(is.numeric, ~replace(., is.na(.), 0))
   
   return(df_pivoted)
 }
-
 
 
 
