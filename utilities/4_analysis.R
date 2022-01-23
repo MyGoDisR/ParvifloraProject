6ce7f2fc57cc2ad6859053f450b4c470cfef9f05
MattG133 <maciek.golik07@gmail.com>
2022-01-23 19:18:56 +0100

    -dodano dwie funkcje tworzące bar ploty -jedna tworzy bar plot pokazujący znormalizowany dochód każdego sklepu (0 to średnia total_revenue dla wszystkich kwiatów) i to czy jest powyżej czy poniżej średniej -drugi tworzy bar chart średnich wartości zamówień dla każdego kwiatka, ale sam chart wygląda średnio ponieważ średnia dla Daffodils jest nieporównanie wyższa niż dla reszty - nie mam zbytnio pomysłu na ten chart na tą chwilę, więc nie wiem czy do czegokolwiek się przyda

--- utilities/4_analysis.R
+++ utilities/4_analysis.R
@@ -56,3 +56,63 @@ gowno_plot <- function(df, period){
   save_plot("blabla.png")
 }
 
+diverging_bar_stores <- function(df) {
+  '
+  Function creates Diverging bar chart as shown here: http://r-statistics.co/Top50-Ggplot2-Visualizations-MasterList-R-Code.html
+  Input:
+    -> Parviflora dataframe in its final form
+  Output:
+    <- png. file with the chart
+  '
+  
+  #preparing data
+  df_plot_prep <- df %>%
+    dplyr::group_by(store_name) %>% 
+    dplyr::summarise(revenue=sum(rev_total)) %>% #grouping by store_name and summarizing with sum of total revenues for the stores
+    mutate(revenue_norm = round((revenue - mean(revenue))/sd(revenue), 2)) %>% #calculating normalized revenue equation and rounding to two decimal places
+    mutate(flag = case_when(revenue_norm < 0 ~ "below", revenue_norm > 0 ~ "above")) %>% #creating flag column with "above" for stores with higher revenue than average and "below" for lower
+    dplyr::arrange(revenue_norm) %>% #sorting by revenue_norm
+    dplyr::mutate(store_name = factor(store_name, level=store_name)) #converting store_name to factor, because the plot doesn't sort bars properly without it
+  
+  
+  # Diverging Barcharts
+  ggplot(df_plot_prep, aes(x=store_name, y=revenue_norm, label=revenue_norm)) + 
+    geom_bar(stat='identity', aes(fill=flag), width=.5)  +
+    scale_fill_manual(name="Revenue", 
+                      labels = c("Above Average", "Below Average"), 
+                      values = c("above"="#53AD70", "below"="#FA9391")) + 
+    labs(subtitle="Normalized Revenue per Store (0 = average revenue)", 
+         title= "Revenues") + 
+    theme(plot.title = element_text(hjust = 0.5), plot.subtitle = element_text(hjust = 0.5))  +
+    coord_flip()
+  
+  save_plot("Normalized_Revenue_per_Store.png")
+}
+
+
+bar_order_flower <- function(df) {
+  '
+  Function first creates data with mean order value for each flower (revenues / count), then plots it
+  NIE MAM ZBYTNIO POMYSŁU NA TEN WYKRES, BO NA TĄ CHWILĘ WYGLĄDA ŚREDNIO XD
+  Input:
+    -> Parviflora dataframe in its final form
+  Output:
+    <- png. file with the chart
+  '
+  
+  df_order <- df_analysis %>%
+    dplyr::mutate(Azalea = rev_Azalea/count_Azalea,
+                  Begonia = rev_Begonia/count_Begonia,
+                  Carnation = rev_Carnation/count_Carnation,
+                  Daffodil = rev_Daffodil/count_Daffodil) %>%
+    tidyr::pivot_longer(Azalea:Daffodil, names_to = 'flower', values_to = 'order_value')  %>%
+    dplyr::group_by(flower) %>%
+    dplyr::summarize(mean_order_value = mean(order_value, na.rm = TRUE))
+
+  ggplot(df_order, aes(x = flower, y = mean_order_value)) + geom_bar(stat = "identity")
+  
+  save_plot("Mean_Order_per_Flower.png")
+  
+}
+
+
